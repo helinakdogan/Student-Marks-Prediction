81f07acd1572aa8319e89d2f499442cdb16d19a0
Helin Akdoğan <akdoganhelin@gmail.com>
2024-04-05 02:27:35 +0300

    .

--- test.py
+++ test.py
@@ -1,7 +1,23 @@
-import sklearn.linear_model 
-import LinearRegression
+from sklearn.linear_model import LinearRegression
 import pandas as pd
 
-df = pd.read.csv('student.csv')
+# Verileri yükle
+df = pd.read_csv('student.csv')
 
-df.head(3)
+# Veri çerçevesinin başını göster
+print(df.head(3))
+
+# Veri çerçevesinin genel bilgilerini göster
+print(df.info())
+
+# Bağımsız değişkenler (x) ve bağımlı değişken (y) seçimi
+y = df[['Marks']]
+x = df[['number_courses', 'time_study']]
+
+# Modeli eğit
+l = LinearRegression()
+model = l.fit(x, y)
+
+# Tahmin yap ve sonuçları göster
+prediction = model.predict([[4, 4]])
+print("Tahmin sonucu:", prediction)
